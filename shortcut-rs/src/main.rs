ff8243d9ef030d8fe4f2f1be08ff25baafb3be6d
Willem Olding <willemolding@gmail.com>
2022-10-08 11:02:53 -0500

    adds rust CLI that imports bacon

---
+++ shortcut-rs/src/main.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+const PREIMAGE_CACHE_DIR: &str = "../preimage-cache";
+
+const VALIDATORS_ROOT_HEX_STR: &str = "0x4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95";
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let prev_update = load_hash(&args[1]);
+    let update = load_hash(&args[2]);
+
+    let validators_root = hex::decode(VALIDATORS_ROOT_HEX_STR).unwrap();
+    bacon::process_sync_committee_period_update(prev_update, update, validators_root)?;
+
+    Ok(())
+}
+
+fn load_hash(hash: &str) -> Vec<u8> {
+    let mut f = File::open(format!("{}/{}", PREIMAGE_CACHE_DIR, hash)).unwrap();
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).unwrap();
+    buffer
+}
